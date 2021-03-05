7f12874207400dcf10191461981940e449441b48
Rafael Barbosa <rafael.1940.b@gmail.com>
2021-03-05 19:44:32 -0300

    Muda forma de selecionar o tipo da implementação

--- src/pilha.cpp
+++ src/pilha.cpp
@@ -8,7 +8,13 @@ using namespace std;
 //define os tipos aceitos na pilha
 template class Stack<int>;
 
-//-----------------Pilha com array [INICIO]-------------------
+//define o tipo da pilha
+#define ARRAY 0
+#define LISTA_LINKADA 1
+#define TIPO_PILHA ARRAY
+
+
+#if TIPO_PILHA == ARRAY
 
 template<typename Type>
 Type *stack; //array para a pilha
@@ -83,84 +89,83 @@ bool Stack<Type>::isNotEmpty()
     return (topPosition >= 0);
 }
 
-//-----------------Pilha com array [FIM]-------------------
+#endif
 
-//-----------------Pilha com lista linkada [INICIO]--------
-
-// template<typename Type>
-// class StackNode {
-//     public:
-//         Type data;
-//         StackNode* next;
-// };
+#if TIPO_PILHA == LISTA_LINKADA
+template<typename Type>
+class StackNode {
+    public:
+        Type data;
+        StackNode* next;
+};
  
-// template<typename Type>
-// StackNode<Type>* newNode(Type data)
-// {
-//     StackNode<Type>* stackNode = new StackNode<Type>();
-//     stackNode->data = data;
-//     stackNode->next = NULL;
-//     return stackNode;
-// }
+template<typename Type>
+StackNode<Type>* newNode(Type data)
+{
+    StackNode<Type>* stackNode = new StackNode<Type>();
+    stackNode->data = data;
+    stackNode->next = NULL;
+    return stackNode;
+}
 
-// template<typename Type>
-// StackNode<Type>* topNode;
+template<typename Type>
+StackNode<Type>* topNode;
 
-// template<typename Type>
-// Stack<Type>::Stack()
-// {
-// }
+template<typename Type>
+Stack<Type>::Stack()
+{
+}
 
-// template<typename Type>
-// bool Stack<Type>::isEmpty()
-// {
-//     return !topNode<Type>;
-// }
+template<typename Type>
+bool Stack<Type>::isEmpty()
+{
+    return !topNode<Type>;
+}
 
-// template<typename Type>
-// bool Stack<Type>::isNotEmpty()
-// {
-//     return topNode<Type>;
-// }
+template<typename Type>
+bool Stack<Type>::isNotEmpty()
+{
+    return topNode<Type>;
+}
 
-// template<typename Type>
-// void Stack<Type>::push( Type data )
-// {
-//     StackNode<Type>* stackNode = newNode(data);
-//     stackNode->next = topNode<Type>;
-//     topNode<Type> = stackNode;
-// }
+template<typename Type>
+void Stack<Type>::push( Type data )
+{
+    StackNode<Type>* stackNode = newNode(data);
+    stackNode->next = topNode<Type>;
+    topNode<Type> = stackNode;
+}
 
-// template<typename Type>
-// Type Stack<Type>::pop()
-// {
-//     if (Stack<Type>::isEmpty()){
-//         return NULL;
-//     }
+template<typename Type>
+Type Stack<Type>::pop()
+{
+    if (Stack<Type>::isEmpty()){
+        return NULL;
+    }
 
-//     StackNode<Type>* aux = topNode<Type>;
-//     topNode<Type> = topNode<Type>->next;
+    StackNode<Type>* aux = topNode<Type>;
+    topNode<Type> = topNode<Type>->next;
 
-//     Type popped = aux->data;
-//     free(aux);
+    Type popped = aux->data;
+    free(aux);
  
-//     return popped;
-// }
+    return popped;
+}
 
-// template<typename Type>
-// Type Stack<Type>::top()
-// {
-//     if (Stack<Type>::isEmpty())
-//         return NULL;
-//     return topNode<Type>->data;
-// }
+template<typename Type>
+Type Stack<Type>::top()
+{
+    if (Stack<Type>::isEmpty())
+        return NULL;
+    return topNode<Type>->data;
+}
 
-// template<typename Type>
-// Stack<Type>::~Stack()
-// {
-//     while(Stack<Type>::isNotEmpty()){
-//         pop();
-//     }
-// }
+template<typename Type>
+Stack<Type>::~Stack()
+{
+    while(Stack<Type>::isNotEmpty()){
+        pop();
+    }
+}
 
-//-----------------Pilha com lista linkada [FIM]--------
+#endif
